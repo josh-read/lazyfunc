b46ae35badf7cb5ed5d340c3d1d362a80ff7f993
Josh Read <joshua-read@hotmail.co.uk>
2022-12-24 14:39:06 +0000

    Refactor new description generation logic from add, mul and truediv into separate method

--- src/mfunc/math_func.py
+++ src/mfunc/math_func.py
@@ -9,6 +9,10 @@ def callable_name(func):
         return func.__class__.__name__
 
 
+def add_parentheses(s):
+    return ''.join(['(', s, ')'])
+
+
 class MathFunc:
 
     def __init__(self, func, *args, description=None, **kwargs):
@@ -40,44 +44,37 @@ class MathFunc:
                   f"Must be either callable or a scalar value."
             raise TypeError(msg)
 
+    def new_description_from_operation(self, other, operation_symbol, commutative):
+        self_desc = self.description
+        if isinstance(other, MathFunc):
+            other_desc = other.description
+        elif callable(other):
+            other_desc = callable_name(other)
+        else:
+            other_desc = str(other)
+
+        if not commutative:
+            # Adding brackets around each operand is the only way to ensure correctness with the current implementation.
+            # However, it is not desirable as it produces a bunch of unnecessary brackets. This problem can be alleviated
+            # if this function had knowledge of all previous operations.
+            self_desc, other_desc = [add_parentheses(desc) for desc in (self_desc, other_desc)]
+
+        return ' '.join((self_desc, operation_symbol, other_desc))
+
     def __add__(self, other):
         """Add self to a scalar value or any other callable including another MathFunc instance."""
         new_func = self.new_func_from_operation(other, operator.add)
-
-        if isinstance(other, MathFunc):
-            new_desc = self.description + ' + ' + other.description
-        elif callable(other):
-            new_desc = self.description + ' + ' + callable_name(other)
-        else:
-            new_desc = self.description + ' + ' + str(other)
-
+        new_desc = self.new_description_from_operation(other, '+', commutative=True)
         return MathFunc(func=new_func, description=new_desc)
 
     def __mul__(self, other):
         """Multiply self with a scalar value or any other callable including another MathFunc instance."""
         new_func = self.new_func_from_operation(other, operator.mul)
-
-        if isinstance(other, MathFunc):
-            new_desc = self.description + ' * ' + other.description
-        elif callable(other):
-            new_desc = self.description + ' * ' + callable_name(other)
-        else:
-            new_desc = self.description + ' * ' + str(other)
-
+        new_desc = self.new_description_from_operation(other, '*', commutative=True)
         return MathFunc(func=new_func, description=new_desc)
 
     def __truediv__(self, other):
         """Multiply self with a scalar value or any other callable including another MathFunc instance."""
         new_func = self.new_func_from_operation(other, operator.truediv)
-
-        # Adding brackets around each operand is the only way to ensure correctness with the current implementation.
-        # However, it is not desirable as it produces a bunch of unnecessary brackets. This problem can be alleviated
-        # if this function had knowledge of all previous operations.
-        if isinstance(other, MathFunc):
-            new_desc = '(' + self.description + ') / (' + other.description + ')'
-        elif callable(other):
-            new_desc = '(' + self.description + ') / (' + callable_name(other) + ')'
-        else:
-            new_desc = '(' + self.description + ') / (' + str(other) + ')'
-
+        new_desc = self.new_description_from_operation(other, '/', commutative=False)
         return MathFunc(func=new_func, description=new_desc)
