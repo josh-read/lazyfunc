52a91dfb9f00ae25dec71538dd602a6c75229c4a
Josh Read <joshua-read@hotmail.co.uk>
2022-12-29 18:22:19 +0000

    Improve test function names

--- tests/test_math_func.py
+++ tests/test_math_func.py
@@ -4,66 +4,66 @@ import pytest
 from mfunc import MathFunc
 
 
-def f(x):
+def single_parameter_function(x):
     return x
 
 
-def g(x, y):
+def multi_parameter_function(x, y):
     return x + y
 
 
-class H:
+class SingleParameterClass:
 
     def __call__(self, x):
         return x
 
 
-class J:
+class MultiParameterClass:
 
     def __call__(self, x, y):
         return x + y
 
 
 @MathFunc
-def k(x):
+def decorated_math_func(x):
     return 3 * x
 
 
 def test_repr():
-    mf_f = MathFunc(f)
-    assert str(mf_f) == 'MathFunc(f)'
-    mf_f_with_desc = MathFunc(f, description='my_function')
-    assert str(mf_f_with_desc) == 'MathFunc(my_function)'
-    mf_j = MathFunc(J())
-    assert str(mf_j) == 'MathFunc(J)'
+    mf_f = MathFunc(single_parameter_function)
+    assert str(mf_f) == 'MathFunc(single_parameter_function)'
+    mf_f_with_desc = MathFunc(single_parameter_function, description='my_single_parameter_function')
+    assert str(mf_f_with_desc) == 'MathFunc(my_single_parameter_function)'
+    mf_j = MathFunc(MultiParameterClass())
+    assert str(mf_j) == 'MathFunc(MultiParameterClass)'
 
 
 def test_call():
     x = np.array([1, 2, 3, 4])
     y = np.random.rand(4)
     print(y)
-    mf_f = MathFunc(f)
+    mf_f = MathFunc(single_parameter_function)
     assert np.allclose(mf_f(x), x)
-    mf_j = MathFunc(J())
+    mf_j = MathFunc(MultiParameterClass())
     assert np.allclose(mf_j(x, y), (x + y))
 
 
 def test_mul():
     # test two MathFunc objects
     x = np.random.rand(4)
-    mf_f = MathFunc(f)
-    mf_h = MathFunc(H())
+    mf_f = MathFunc(single_parameter_function)
+    mf_h = MathFunc(SingleParameterClass())
     mf_fh = mf_f * mf_h
-    assert str(mf_fh) == 'MathFunc(f * H)'
+    assert str(mf_fh) == 'MathFunc(single_parameter_function * SingleParameterClass)'
     assert np.allclose(mf_fh(x), x**2)
     # test a MathFunc object with a normal function
-    mf_g = MathFunc(g)
-    mf_gj = mf_g * J()
-    assert str(mf_gj) == 'MathFunc(g * J)'
+    mf_g = MathFunc(multi_parameter_function)
+    mf_gj = mf_g * MultiParameterClass()
+    assert str(mf_gj) == 'MathFunc(multi_parameter_function * MultiParameterClass)'
     assert np.allclose(mf_gj(x, x), 4*x**2)
     # test a MathFunc object with a scalar
     mf_f2 = mf_f * 2
-    assert str(mf_f2) == 'MathFunc(f * 2)'
+    assert str(mf_f2) == 'MathFunc(single_parameter_function * 2)'
     assert np.allclose(mf_f2(x), 2*x)
     with pytest.raises(TypeError):
         mf_f * 'foo'
@@ -75,19 +75,19 @@ def test_mul():
 def test_add():
     # test two MathFunc objects
     x = np.random.rand(4)
-    mf_f = MathFunc(f)
-    mf_h = MathFunc(H())
+    mf_f = MathFunc(single_parameter_function)
+    mf_h = MathFunc(SingleParameterClass())
     mf_fh = mf_f + mf_h
-    assert str(mf_fh) == 'MathFunc(f + H)'
+    assert str(mf_fh) == 'MathFunc(single_parameter_function + SingleParameterClass)'
     assert np.allclose(mf_fh(x), 2*x)
     # test a MathFunc object with a normal function
-    mf_g = MathFunc(g)
-    mf_gj = mf_g + J()
-    assert str(mf_gj) == 'MathFunc(g + J)'
+    mf_g = MathFunc(multi_parameter_function)
+    mf_gj = mf_g + MultiParameterClass()
+    assert str(mf_gj) == 'MathFunc(multi_parameter_function + MultiParameterClass)'
     assert np.allclose(mf_gj(x, x), 4*x)
     # test a MathFunc object with a scalar
     mf_f2 = mf_f + 2
-    assert str(mf_f2) == 'MathFunc(f + 2)'
+    assert str(mf_f2) == 'MathFunc(single_parameter_function + 2)'
     assert np.allclose(mf_f2(x), x + 2)
     with pytest.raises(TypeError):
         mf_f + 'foo'
@@ -96,36 +96,23 @@ def test_add():
 def test_radd():
     # test two MathFunc objects
     x = np.random.rand(4)
-    mf_f = MathFunc(f)
-    mf_h = MathFunc(H())
+    mf_f = MathFunc(single_parameter_function)
+    mf_h = MathFunc(SingleParameterClass())
     mf_fh = mf_f + mf_h
-    assert str(mf_fh) == 'MathFunc(f + H)'
+    assert str(mf_fh) == 'MathFunc(single_parameter_function + SingleParameterClass)'
     assert np.allclose(mf_fh(x), 2*x)
     # test a MathFunc object with a normal function
-    mf_g = MathFunc(g)
-    mf_jg = J() + mf_g
-    assert str(mf_jg) == 'MathFunc(J + g)'
+    mf_g = MathFunc(multi_parameter_function)
+    mf_jg = MultiParameterClass() + mf_g
+    assert str(mf_jg) == 'MathFunc(MultiParameterClass + multi_parameter_function)'
     assert np.allclose(mf_jg(x, x), 4*x)
     # test a MathFunc object with a scalar
     mf_2f = 2 + mf_f
-    assert str(mf_2f) == 'MathFunc(2 + f)'
+    assert str(mf_2f) == 'MathFunc(2 + single_parameter_function)'
     assert np.allclose(mf_2f(x), x + 2)
     with pytest.raises(TypeError):
         mf_f + 'foo'
 
 
-def test_truediv():
-    x = 1
-    mf_f = MathFunc(f)
-    tricky_eq1 = mf_f + mf_f / (mf_f + 2)
-    expected_str1 = 'MathFunc(f + f / (f + 2))'
-    assert np.allclose(tricky_eq1(x), x + x / (x + 2))
-    assert str(tricky_eq1) == expected_str1
-    tricky_eq2 = (mf_f + mf_f) / (mf_f + 2)
-    expected_str2 = 'MathFunc((f + f) / (f + 2))'
-    assert np.allclose(tricky_eq2(x), (x + x) / (x + 2))
-    assert str(tricky_eq2) == expected_str2
-
-
 def test_decorated_function():
-    assert (k + f)(1) == 4
+    assert (decorated_math_func + single_parameter_function)(1) == 4
