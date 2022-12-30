fe74e18658b1cf52160e127d3d75fbed105726fa
Josh Read <joshua-read@hotmail.co.uk>
2022-12-30 08:53:28 +0000

    Split big test_equations function into individual functions

--- tests/test_equations.py
+++ tests/test_equations.py
@@ -1,3 +1,6 @@
+"""Test combining operations of different rank."""
+
+
 from mfunc import *
 
 
@@ -16,25 +19,31 @@ def three(x):
     return 3
 
 
-def test_equations():
-    """Test combining operations of different rank."""
-    # no brackets different rank
+def test_equation_1():
     eq = one + one / two
     assert str(eq) == 'MathFunc(one + one / two)'
     assert eq(None) == 1 + 1 / 2
-    # brackets different rank
+
+
+def test_equation_2():
     eq = (one + one) / two
     assert str(eq) == 'MathFunc((one + one) / two)'
     assert eq(None) == (1 + 1) / 2
-    # brackets different rank
+
+
+def test_equation_3():
     eq = two + (one + one) / two + three
     assert str(eq) == 'MathFunc(two + (one + one) / two + three)'
     assert eq(None) == 2 + (1 + 1) / 2 + 3
-    # brackets different rank
+
+
+def test_equation_4():
     eq = (two + (one + one) / two + three) ** two
     assert str(eq) == 'MathFunc((two + (one + one) / two + three) ** two)'
     assert eq(None) == (2 + (1 + 1) / 2 + 3) ** 2
-    # test bracketing for ascending then descending rank
+
+
+def test_equation_5():
     eq = ((one + one) * two / two) ** (three / one) + one
     assert str(eq) == 'MathFunc(((one + one) * two / two) ** (three / one) + one)'
     assert eq(None) == ((1 + 1) * 2 / 2) ** (3 / 1) + 1
