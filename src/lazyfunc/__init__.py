6401f64b9a158c32807e35d33cbe733783a7f353
Josh Read <53308348+josh-read@users.noreply.github.com>
2023-01-06 10:57:00 +0000

    Update src/lazyfunc/__init__.py
    Co-authored-by: Jonathan Shimwell <drshimwell@gmail.com>

--- src/lazyfunc/__init__.py
+++ src/lazyfunc/__init__.py
@@ -1,7 +1,7 @@
 from importlib.metadata import version
 
-__version__ = version("lazyfunc")
-
-__all__ = ["__version__"]
-
 from .lazy_func import LazyFunc
+
+
+__version__ = version("lazyfunc")
+__all__ = ["__version__", "LazyFunc"]
